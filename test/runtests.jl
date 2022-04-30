dfa356997265637886843d5ad7451a82f60f88f9
Jerry Ling <proton@jling.dev>
2022-04-30 01:14:24 -0400

    add some basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
-using LiteHF
+using LiteHF, Optim
 using Test
 
-@testset "LiteHF.jl" begin
+function testload(path)
+    pydict = load_pyhfjson(path)
+    pyhfmodel = build_pyhf(pydict)
+    res = maximize(pyhfmodel.LogLikelihood, pyhfmodel.prior_inits,
+                   NelderMead(), Optim.Options(iterations=3*10^4);)
+    best_paras = Optim.maximizer(res)
+    Dict(pyhfmodel.prior_names .=> best_paras)
+    twice_nll = -2*pyhfmodel.LogLikelihood(best_paras)
+end
+
+@testset "Full model" begin
     # Write your tests here.
+    @test isapprox(testload(joinpath(@__DIR__, "./pyhfjson/single_channel_big.json")), 80.67893633848638;
+                  rtol=0.0001)
 end
